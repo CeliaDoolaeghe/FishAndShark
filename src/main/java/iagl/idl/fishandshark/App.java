c77aac8521f7ae6d2d3b2c7f5fe334239927ca30
CeliaDoolaeghe <celia.cacciatore@gmail.com>
2015-01-06 16:57:52 +0100

    Initial import

---
+++ src/main/java/iagl/idl/fishandshark/App.java
@@ -0,0 +1,13 @@
+package iagl.idl.fishandshark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
