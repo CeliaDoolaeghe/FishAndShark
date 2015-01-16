1c6d4c0f80b9c98d551c42ba8b2e7e6e2d5e649c
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2015-01-16 19:33:40 +0100

    improvement: make the StatusPanel generic
    Make the StatusPanel generic by grouping Agents by color.
    Count the number of Agents for each color, and display each counter.

--- src/main/java/iagl/idl/simulation/view/StatusPanel.java
+++ src/main/java/iagl/idl/simulation/view/StatusPanel.java
@@ -1,12 +1,12 @@
 package iagl.idl.simulation.view;
 
 import iagl.idl.simulation.mas.MAS;
+import iagl.idl.simulation.mas.agent.Agent;
 import iagl.idl.simulation.mas.environment.Environment;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.*;
 
 /**
  * Display MAS status.
@@ -18,43 +18,69 @@ public class StatusPanel extends JPanel implements Observer {
 	private static final long serialVersionUID = 1L;
 	private MAS mas;
     private JLabel chrononsLabel;
-    private JLabel fishLabel;
-    private JLabel sharksLabel;
+    private Map<Color, JLabel> counterLabels;
 
     public StatusPanel(MAS mas) {
         super();
         this.mas = mas;
 
         // Subscribe to Environment Observation
-        Environment environment = mas.getEnvironment();
+        Environment<Agent> environment = mas.getEnvironment();
         environment.addObserver(this);
 
         // Create Labels
         chrononsLabel = new JLabel(String.valueOf(mas.getScheduling()));
-        fishLabel = new JLabel(/*String.valueOf(environment.getFish())*/);
-        sharksLabel = new JLabel(/*String.valueOf(environment.getSharks())*/);
+
+        // Compute colors
+        Set<Color> colors = new HashSet<>();
+        for(Agent agent : environment.getAllAgents()) {
+            colors.add(agent.getColor());
+        }
+
+        // Create a label for each color
+        Map<Color, JLabel> colorLabels = new HashMap<>();
+        counterLabels = new HashMap<>();
+        for(Color color : colors) {
+            colorLabels.put(color, new JLabel());
+            counterLabels.put(color, new JLabel());
+        }
 
         // Add labels to Panel
         GridLayout layout = new GridLayout(0, 2);
         setLayout(layout);
-        add(new JLabel("Chronons:"));
-        add(chrononsLabel);
-        JLabel textFishLabel = new JLabel("Fish:");
-        textFishLabel.setForeground(Color.BLUE);
-        add(textFishLabel);
-        add(fishLabel);
-        JLabel textSharksLabel = new JLabel("Sharks:");
-        textSharksLabel.setForeground(Color.RED);
-        add(textSharksLabel);
-        textFishLabel.setForeground(Color.BLUE);
-        add(sharksLabel);
+        JLabel colorLabel;
+        for(Color color : colors) {
+            colorLabel = colorLabels.get(color);
+            colorLabel.setOpaque(true);
+            colorLabel.setBackground(color);
+            add(colorLabel);
+            add(counterLabels.get(color));
+        }
     }
 
     @Override
     public void update(Observable observable, Object o) {
-        Environment environment = mas.getEnvironment();
+        Environment<Agent> environment = mas.getEnvironment();
         chrononsLabel.setText(String.valueOf(mas.getScheduling()));
-//        fishLabel.setText(String.valueOf(environment.getFish()));
-//        sharksLabel.setText(String.valueOf(environment.getSharks()));
+
+        // Compute the number of Agents for each color
+        Color currentColor;
+        int nbAgentsForColor;
+        Map<Color, Integer> counter = new HashMap<>();
+        for(Agent agent : environment.getAllAgents()) {
+            currentColor = agent.getColor();
+            if(counter.containsKey(currentColor)) {
+                nbAgentsForColor = counter.get(currentColor) + 1;
+            }
+            else {
+                nbAgentsForColor = 1;
+            }
+            counter.put(currentColor, nbAgentsForColor);
+        }
+
+        // Update each JLabel
+        for(Color color: counter.keySet()) {
+            counterLabels.get(color).setText(String.valueOf(counter.get(color)));
+        }
     }
 }
