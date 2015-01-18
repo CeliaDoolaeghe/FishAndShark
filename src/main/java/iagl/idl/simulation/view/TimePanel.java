c1028e20f86276efa03f0b01384983c15314ec5a
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2015-01-18 09:20:46 +0100

    feat: Add Time panel

---
+++ src/main/java/iagl/idl/simulation/view/TimePanel.java
@@ -0,0 +1,52 @@
+package iagl.idl.simulation.view;
+
+import iagl.idl.simulation.mas.MAS;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+/**
+ *  @author Célia Cacciatore, Jonathan Geoffroy
+ */
+public class TimePanel extends JPanel {
+    private final MAS mas;
+
+    public TimePanel(MAS mas) {
+        super();
+        this.mas = mas;
+
+        final JButton startPause = new JButton("Pause");
+        startPause.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if(TimePanel.this.mas.isTerminated()) {
+                    try {
+                        TimePanel.this.mas.run();
+                        startPause.setText("Pause");
+                    } catch (InterruptedException e) {
+                        System.exit(-1);
+                    }
+                }
+                else {
+                    TimePanel.this.mas.setTerminated(true);
+                    startPause.setText("Start");
+                }
+            }
+        });
+
+        JButton stop = new JButton("Stop");
+        stop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                TimePanel.this.mas.setTerminated(true);
+                JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(TimePanel.this);
+                topFrame.dispatchEvent(new WindowEvent(topFrame, WindowEvent.WINDOW_CLOSING));
+            }
+        });
+
+        this.add(startPause);
+        this.add(stop);
+    }
+}
